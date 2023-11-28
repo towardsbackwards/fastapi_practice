907f87782f75931310be951c764862a09fd1ac93
Pavel Martynov <python_dev@omnicdek.ru>
2023-11-28 19:50:58 +0300

    response exception handled, some nests in schemas

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import ResponseValidationError
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 
 app = FastAPI(
     title='Testing App'
 )
 
+
+@app.exception_handler(ResponseValidationError)
+async def validation_exception_handler(request: Request, exc: ResponseValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": exc.errors()}
+    )
+
+
 fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": "admin", "name": ["Bob"],
+     "degree": [{"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}]},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"}
 ]
+
+
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
 
 
 class User(BaseModel):
     id: int
     role: str
     name: str
+    degree: Optional[List[Degree]] = []
 
 
 @app.get("/users/{user_id}", response_model=List[User])
@@ -25,7 +51,7 @@ def hello(user_id: int):
     resp = [u for u in fake_users if u.get("id") == user_id]
     if resp:
         return resp[0]
-    return 'User not found'
+    return {"data": 'User not found'}
 
 
 fake_trades = [
